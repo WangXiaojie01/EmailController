89b465f1e34ab11a3b9299c6517a17a9417274d9
WangXiaojie01 <1259488569@qq.com>
2020-03-17 20:18:19 +0800

    添加源码

--- Code/EmailController.py
+++ Code/EmailController.py
@@ -2,21 +2,22 @@
 #-*- coding: UTF-8 -*- 
 
 '''
-copyright @wangxiaojie 2020.03/12
+copyright @wangxiaojie 2020.03.12
 author: wangxiaojie
 '''
 
 import os, sys
 
-emailSenderDir = os.path.abspath(os.path.join(__file__, "..", "EmailSender"))
-if os.path.exists(emailSenderDir):
-    sys.path.append(emailSenderDir)
-JsonUtilDir = os.path.abspath(os.path.join(__file__, "..", "JsonUtil"))
-if os.path.exists(JsonUtilDir):
-    sys.path.append(JsonUtilDir)
+codeArray = ["EmailSender", "JsonUtil", "FileUtil"]
+
+for codeName in codeArray:
+    sourceCodeDir = os.path.abspath(os.path.join(__file__, "..", codeName))
+    if os.path.exists(sourceCodeDir):
+        sys.path.append(sourceCodeDir)
 
 from EmailSender import *
 from JsonUtil import *
+from FileUtil import *
 
 __all__ = [
     "EmailController"
@@ -27,7 +28,7 @@ class EmailController(object):
         self.emailHostList = None
         self.emailSendersList = None
         self.emailReceiversList = None 
-        self.nameEmialMapList = None
+        self.nameEmailMapList = None
         self.emailHost = None 
         self.emailSenders = None 
         self.emailSender = None 
@@ -40,36 +41,36 @@ class EmailController(object):
         self.nameEmailMap = None
         self.hasInit = False
         self.emailConfFile = None
-
-    def __init__(self, emailHost, emailSender, emailAuth, emailReceivers, toStr):
+    
+    def init(self, emailHost, emailSender, emailAuth, emailReceivers, toStr):
         self.emailHost = emailHost
         self.emailSender = emailSender
         self.emailAuth = emailAuth
         self.emailReceivers = emailReceivers
         self.emailToStr = toStr
         self.hasInit = True
-
-    def __init__(self, configFile):
+    
+    def initWithConfigFile(self, configFile):
         if not os.path.isfile(configFile):
             emailLogger.error('%s is not a file' % configFile)
             return False
         result, emailJsonDict = getJsonFromFile(configFile)
         if result:
             if "email_hosts" in emailJsonDict:
-                self.emailHost = emailJsonDict["email_hosts"]
+                self.emailHostList = emailJsonDict["email_hosts"]
             if "email_senders" in emailJsonDict:
                 self.emailSendersList = emailJsonDict["email_senders"]
             if "email_receivers" in emailJsonDict:
                 self.emailReceiversList = emailJsonDict["email_receivers"]
             if "name_email_map" in emailJsonDict:
-                self.nameEmialMapList = emailJsonDict["name_email_map"]
+                self.nameEmailMapList = emailJsonDict["name_email_map"]
             self.hasInit = True
             self.emailConfFile = configFile
             return True
         else:
             emailLogger.error('getJsonFromFile(%s) error' % configFile)
             return False
-
+    
     def sendEmail(self):
         result = sendEmail(self.emailHost, self.emailSender, self.emailAuth, self.emailReceivers, self.emailToStr, self.emailSubject, self.emailContent, self.emailAttachDict)
         if not result:
@@ -81,7 +82,7 @@ class EmailController(object):
         if self.hasInit:
             if self.emailHostList == None or \
                 self.emailSendersList == None or \
-                self.emailReceiversList == None
+                self.emailReceiversList == None:
                 emailLogger.error('email_hosts or email_senders or email_receivers is None in %s' % self.emailConfFile)
                 return False
             self.emailHost, result = getDefaultFromJsonObj(tag, self.emailHostList)
@@ -95,7 +96,7 @@ class EmailController(object):
             if result != "success": 
                 emailLogger.error("get %s from email_senders failed" % tag)
                 return False
-             if self.emailSenders == None or len(self.emailSenders) == 0:
+            if self.emailSenders == None or len(self.emailSenders) == 0:
                 emailLogger.error("%s in email_senders is None or empty" % tag)
                 return False 
             self.emailReceivers, result = getDefaultFromJsonObj(tag, self.emailReceiversList)
@@ -106,9 +107,9 @@ class EmailController(object):
                 and (otherReceivers == None or len(otherReceivers) == 0):
                 emailLogger.error("both %s in email_receivers and otherReceivers is None or empty")
                 return False
-            if self.nameEmialMapList != None and len(self.nameEmialMapList) > 0:
-                self.nameEmailMap, result = getDefaultFromJsonObj(tag, self.nameEmialMapList)
-            self.combineEmailReceivers(otherReceive)
+            if self.nameEmailMapList != None and len(self.nameEmailMapList) > 0:
+                self.nameEmailMap, result = getDefaultFromJsonObj(tag, self.nameEmailMapList)
+            self.combineEmailReceivers(self.emailReceivers, otherReceivers)
             if self.emailReceivers == None or len(self.emailReceivers) == 0:
                 emailLogger.error("noting in receivers after combineEmailReceivers")
                 return False
@@ -117,7 +118,7 @@ class EmailController(object):
             self.emailContent = content
             self.emailAttachDict = attach
             sendResult = False
-            for sender, auth in self.emailSenders:
+            for sender, auth in self.emailSenders.items():
                 self.emailSender = sender
                 self.emailAuth = auth
                 if self.sendEmail():
@@ -130,9 +131,9 @@ class EmailController(object):
             emailLogger.error("EmailController has not inited")
             return False
 
-    def combineEmailReceivers(self, otherReceivers):
-        receivers1 = analyzeReceiverStr(self.emailReceivers)
-        receivers2 = analyzeReceiverStr(otherReceivers)
+    def combineEmailReceivers(self, receivers, otherReceivers):
+        receivers1 = self.analyzeReceiverStr(receivers)
+        receivers2 = self.analyzeReceiverStr(otherReceivers)
         self.emailReceivers = combineUnitList(receivers1, receivers2)
         
     def analyzeReceiverStr(self, receivers):
@@ -142,7 +143,7 @@ class EmailController(object):
                 newReceivers = newReceivers.replace(",,", ",")
             receivers = newReceivers.split(",")
         if isinstance(receivers, list):
-            newRecievers = []
+            newReceivers = []
             for i, val in enumerate(receivers):
                 if val in self.nameEmailMap:
                     newReceivers.append(self.nameEmailMap[val])
@@ -153,47 +154,30 @@ class EmailController(object):
         return None
 
     def getEmailToStr(self):
+        self.emailToStr = ",". join(self.emailReceivers)
+        '''
         newStr = ""
         for i, val in enumerate(self.emailReceivers):
             tempStr = "%s<%s>" % (val, val)
-            for key, value in self.nameEmailMap:
+            tempStr = val
+            for key, value in self.nameEmailMap.items():
                 if val == value:
                     tempStr = "%s<%s>" % (key, value)
+                    tempStr = value
                     break
             if newStr == "":
                 newStr = tempStr
             else:
-                newStr = "%s;%s" % (newStr, tempStr)
+                newStr = "%s,%s" % (newStr, tempStr)
         self.emailToStr = newStr
-    
+        '''
+     
     def sendWithTagAndFile(self, tag, subject, contentFile, contentHeader = None, contentTail = None, otherReceivers = None, attach = None):
         if contentFile == None:
             emailLogger.error("contentFile must be assigned with a file")
             return False
-        emailContent = "%s\n%s\%s" % (self.readFile(contentHeader), self.readFile(contentFile), self.readFile(contentTail))
+        emailContent = "%s\n%s\n%s" % (readFile(contentHeader), readFile(contentFile), readFile(contentTail))
         return self.sendEmailWithTag(tag, subject, emailContent, otherReceivers, attach)
 
-    def readFile(self, fileName):
-        if fileName == None or not os.path.isfile(fileName):
-            return ""
-        try:
-            fopen = open(fileName, 'r')
-            content = fopen.read()
-            return content
-        except Exception as e:
-            return ""
-
 if __name__ == "__main__":
-    email_json = os.path.abspath(os.path.join(__file__, "../../etc/email/email.json"))
-    if os.path.exists(email_json):
-        print "exist"
-
-    email_sender = EmailController(email_json)
-
-    attach_path = os.path.abspath(os.path.join(__file__, "../../etc"))
-    email_attach = {os.path.join(attach_path, "README.md"): "test_attach"} #, os.path.join(attach_path, "project/other/monitor.json"): "附件2"}
-    #email_attach = [os.path.join(attach_path, "email_tail.conf"), os.path.join(attach_path, "project/other/monitor.json")]
-    print "is %s"%email_json
-    
-    email_sender.sendEmailWithService("test", "测试邮件", "这是一封测试邮件，如果你收到了，恭喜你，中奖了", None, email_attach, True)
-   
+    pass
